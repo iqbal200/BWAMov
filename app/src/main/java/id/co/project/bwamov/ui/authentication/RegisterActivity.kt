1861d63cfe9b35da954f2bbc057289cf0b49c25e
iqbal <iqbalthamrin200@gmail.com>
2022-09-04 14:26:53 +0800

    initial commit

---
+++ app/src/main/java/id/co/project/bwamov/ui/authentication/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package id.co.project.bwamov.ui.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.co.project.bwamov.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
