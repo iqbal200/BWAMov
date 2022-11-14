8cbb15f4272812dc824c645b95b8a6fbfb277202
iqbal <iqbalthamrin200@gmail.com>
2022-11-14 22:34:21 +0800

    - develop fitur home page, - add three menu fragment, menu dashboard, menu ticket, menu profile

---
+++ app/src/main/java/id/co/project/bwamov/ui/homePage/Film.kt
@@ -0,0 +1,15 @@
+package id.co.project.bwamov.ui.homePage
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+data class Film (
+    var desc: String ?="",
+    var director: String ?="",
+    var genre: String ?="",
+    var judul: String ?="",
+    var poster: String ?="",
+    var rating: String ?=""
+): Parcelable
